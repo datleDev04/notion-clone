c6beb945872d70b71ec61b22d19671eb5df815a2
letiendat040904@gmail.com <letiendat040904@gmail.com>
2024-04-20 16:38:49 +0700

    fix bug delpoy &apos

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -14,7 +14,7 @@ import { useMutation } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { toast } from 'react-toastify'
 import { Button } from '@/components/ui/button'
-import { MoreHorizontalIcon, Trash, Trash2 } from 'lucide-react'
+import { MoreHorizontal, MoreHorizontalIcon, Trash, Trash2 } from 'lucide-react'
 
 
 interface MenuProps {
@@ -44,20 +44,24 @@ const Menu = ({
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button size="sm" variant="ghost" >
-                    <MoreHorizontalIcon />
+            <DropdownMenuTrigger asChild>
+                <Button size="sm" variant="ghost">
+                    <MoreHorizontal className="h-4 w-4" />
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-60" align='end' alignOffset={8} forceMount>
+            <DropdownMenuContent
+                className="w-60"
+                align="end"
+                alignOffset={8}
+                forceMount
+            >
                 <DropdownMenuItem onClick={onArchive}>
-                    <Trash className='w-4 h-4 mr-2' />
+                    <Trash className="h-4 w-4 mr-2" />
                     Delete
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
-
-                <div className='text-xs text-muted-foreground p-2'>
-                    Last edited by : {user?.fullName}
+                <div className="text-xs text-muted-foreground p-2">
+                    Last edited by: {user?.fullName}
                 </div>
             </DropdownMenuContent>
         </DropdownMenu>
